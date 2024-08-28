13b05518dff746b20affbc3db70fe34737c35fc5
Sho Uzaki <sho.uzaki@icloud.com>
2024-08-28 16:56:16 +0900

    fix package.xml and setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'control'
+package_name = 'control_pkg'
 
 setup(
     name=package_name,
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'pub_node = control.pub_node:main'
+            'pub_node = control.pub_node:main',
             'sub_node = control.sub_node:main'
         ],
     },
